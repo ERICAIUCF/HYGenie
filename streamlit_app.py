82f80c527bd184d98853f185bb6f1235c2719d2d
HY Genie <eshresearch@hanyang.ac.kr>
2024-12-11 13:43:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,7 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 HY Genei")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+    "한양대학교 ERICA산학협력단 매뉴얼 기반 인공지능 챗봇입니다."
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
